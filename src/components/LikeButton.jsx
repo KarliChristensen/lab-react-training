8d28eac7c4a61ba7fa9e38da4791ece339fe09b9
Buiscki1 <122836250+Buiscki1@users.noreply.github.com>
2023-06-05 14:36:19 +0200

    Iteration 8

---
+++ src/components/LikeButton.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+function LikeButton() {
+  const color = ['purple', 'blue', 'green', 'yellow', 'orange', 'red'];
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+    const calc = Math.floor(Math.random() * color.length);
+    document.getElementById('like-button').style.backgroundColor = color[calc];
+  };
+
+  return (
+    <div>
+      <button id="like-button" onClick={handleClick}>{count} Likes</button>
+    </div>
+  );
+}
+
+export default LikeButton;
